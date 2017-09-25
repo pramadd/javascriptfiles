3f10be1a64db87b246a5fbdb876d353bc9953910
Pranathi <neethu@codingdojo.com>
2017-09-25 14:34:21 -0700

    i do declare

---
+++ assignment1/idodeclare.js
@@ -0,0 +1,39 @@
+var num= -2;
+console.log("value of num :" +num);
+var num1= 3
+console.log("value of num1 :" +num1);
+
+var num2= 4;
+console.log("value of num2 :" +num2);
+var num4= 4.5;
+console.log("value of num4 :" +num4);
+
+
+var string1 = "types";
+console.log("value of string1 :" +string1);
+
+var string2 ="variables";
+console.log("value of string2 :" +string2);
+
+var string3 ="conditions";
+console.log("value of string2 :" +string2);
+
+var boolean1 ="true";
+console.log("the value of boolean :" +boolean1);
+var boolean2 ="false";
+console.log("the value of boolean :" +string1);
+
+var a;
+console.log("the value of a is " + a);
+
+// function checkUndefined(a){
+//     if(a === undefined){
+//         console.log("value is undefined:", a);
+//     }else{
+//         console.log("value is defined: ", a);
+//     }
+
+// }
+// // calling the func with value x
+// checkUndefined(a);
+
